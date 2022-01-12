71e1ccc90bf64f7e574cd5cba870c04b8b5a9139
Kenneth Giusti <kgiusti@apache.org>
2022-01-12 11:52:23 -0500

    DISPATCH-1487: optimize message annotation composing on message send
    The patch hand-optimizes the creation of the message annotations
    section in an outgoing message.  It implementes the following changes:
    
    o Pre-encode the map keys used by the router annotations.
    o Inline the encoding of related AMQP type and section headers
    o Pre-encode the local router ID for trace and ingress composition
    o minor tweaks to qd_message_send to reduce processing overhead

--- tests/message_test.c
+++ tests/message_test.c
@@ -1607,12 +1607,12 @@ static char *test_local_message_compose(void * context)
         goto exit;
     }
 
-    if (content->ma_count != 4) {
+    if (content->ma_user_count != 4) {
         result = "failed to find user message annotations";
         goto exit;
     }
 
-    if (content->field_user_annotations.length != 52) {
+    if (content->ma_user_annotations.remaining != 52) {
         result = "wrong length of user annotations";
         goto exit;
     }
